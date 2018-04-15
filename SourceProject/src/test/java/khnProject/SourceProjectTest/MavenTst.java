dcb93146bc5a453322dc7e805e56e2d429317efb
Mohiddin <gouse.krnl@gmail.com>
2018-04-15 19:45:30 +0530

    first program

---
+++ SourceProject/src/test/java/khnProject/SourceProjectTest/MavenTst.java
@@ -0,0 +1,10 @@
+package khnProject.SourceProjectTest;
+import org.testng.annotations.Test;
+public class MavenTst {
+	@Test()
+	public void excProjcet(){
+		System.out.println("excProjcet");
+	}
+	
+
+}
